c4cae2a09ce4609f0bf6bd5794902e635440457a
Dmitry_home <79811924115@yandex.ru>
2022-10-10 07:49:54 +0300

    ajs 11 task1 v.2

--- src/js/LoryTeamMember.js
+++ src/js/LoryTeamMember.js
@@ -1,12 +1,13 @@
 export default class LoryTeamMember {
   startDriving = 18;
+
   constructor(name, driveCategory, yearsOfExperianse) {
     this.name = name;
     this.driveCategory = driveCategory;
     this.yearsOfExperianse = yearsOfExperianse;
   }
+
   get teamMembersAge() {
     return `${this.name}у ${this.yearsOfExperianse + this.startDriving} года!`;
   }
 }
-
